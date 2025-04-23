57414b59d7683af5d28c5aa6c9926a09c845db2e
Robloker <juanjuancavalo@gmail.com>
2025-04-23 16:59:38 -0300

    Conteudo principal Completo

---
+++ js/Global-js/Global.js
@@ -0,0 +1,30 @@
+//Animação do menu hamburger abrindo e fechando
+function toggleMenu() {
+
+    const menuToggle = document.getElementById("menu-toggle");
+    const itens = document.getElementById("nav-links");
+
+    // Alterna o menu hamburguer para X
+    menuToggle.classList.toggle("active");
+    
+    if (itens.style.transform === 'translateX(0%)') {
+        itens.style.transform = 'translateX(100%)';
+    } else {
+        itens.style.transform = 'translateX(0%)';
+    }
+}
+
+//Animação do Footer subindo quando o usuario chegar no final do site
+function FtAnimado() {
+    const footer = document.querySelector('footer');
+    const scrollPosition = window.innerHeight + window.pageYOffset;
+    const documentHeight = document.documentElement.scrollHeight;
+
+    if (scrollPosition >= documentHeight) {
+      footer.style.bottom = '0px'; 
+    } else {
+      footer.style.bottom = '-120px'; 
+    }
+  }
+
+  window.addEventListener('scroll', FtAnimado);
