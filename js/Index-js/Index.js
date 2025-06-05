a6731dce351b24fdcfcee719ad39cffb3a4ef391
Juan <juandarocha2020@gmail.com>
2025-06-05 10:07:21 -0300

    Atualização na parte dos monstros e fiz uma wiki um butão que leva para parte de khei

--- js/Index-js/Index.js
+++ js/Index-js/Index.js
@@ -13,3 +13,45 @@ ScrollReveal().reveal('#Wiki-nav', {
     delay: 300,
     reset: false
 });
+
+
+function WorldPortal() { 
+    const gaiaDivs = document.querySelectorAll("#nav-gaia > div");
+    const kheiDivs = document.querySelectorAll("#nav-khei > div");
+
+    const primeiroGaia = gaiaDivs[0];
+    const displayGaia = window.getComputedStyle(primeiroGaia).display;
+
+    if (displayGaia === "flex") {
+        gaiaDivs.forEach(div => div.style.opacity = "0");
+        setTimeout(() => {
+            gaiaDivs.forEach(div => {
+                div.style.display = "none";
+                div.style.opacity = "1";
+            });
+            kheiDivs.forEach(div => {
+                div.style.display = "flex";
+                div.style.opacity = "0";
+                setTimeout(() => {
+                    div.style.opacity = "1";
+                }, 50);
+            });
+        }, 400);
+    } else {
+        kheiDivs.forEach(div => div.style.opacity = "0");
+        setTimeout(() => {
+            kheiDivs.forEach(div => {
+                div.style.display = "none";
+                div.style.opacity = "1";
+            });
+            gaiaDivs.forEach(div => {
+                div.style.display = "flex";
+                div.style.opacity = "0";
+                setTimeout(() => {
+                    div.style.opacity = "1";
+                }, 50);
+            });
+        }, 400);
+    }
+}
+
